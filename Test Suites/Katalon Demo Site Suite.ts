ca94c085e078a71b5741986b60f2752ab4db94a2
sakthi <sakthi@192.168.0.101>
2021-07-25 10:22:30 +0800

    1st Testcase

---
+++ Test Suites/Katalon Demo Site Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Katalon Demo Site Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>776975ab-6b07-42f0-8ad0-eedf03a99cb4</testSuiteGuid>
+</TestSuiteEntity>
